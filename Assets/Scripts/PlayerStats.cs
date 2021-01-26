1d75cec4bed7ee0b2d29e698fd789ba3830814b3
ToniOswin <toni.bgae@gmail.com>
2021-01-26 21:23:35 +0100

    no message

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -6,7 +6,7 @@ public class PlayerStats : MonoBehaviour
 {
     public static float Damage = 1;
     public static float health = 10;
-    public static int money= 800;
+    public static int money;
 
     // Start is called before the first frame update
     void Start()
